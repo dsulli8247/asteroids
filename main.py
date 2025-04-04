2a3eec4cc1e48c3b00eff85ab517bc24ceccc831
David Sullivan <dsulli8247@gmail.com>
2025-04-03 20:02:22 -0400

    "added to main.py and .gitignore"

--- main.py
+++ main.py
@@ -2,12 +2,22 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-import constants
+from constants import *
 
 def main():
+    pygame.init()
     print ("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    number = 2
+    while number < 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        fill()
+        display.flip()
+
 
 if __name__ == "__main__":
     main()
