aa42b68ff8177812e88a995370f14e45c87fd974
David Sullivan <dsulli8247@gmail.com>
2025-04-04 09:41:53 -0400

    updated main

--- main.py
+++ main.py
@@ -6,12 +6,8 @@ from constants import *
 
 def main():
     pygame.init()
-    print ("Starting Asteroids!")
-    print (f"Screen width: {SCREEN_WIDTH}")
-    print (f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    number = 2
-    while number < 1:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
