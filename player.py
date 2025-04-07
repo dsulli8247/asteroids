536cbf5ef1b7c1b350609489e4de841938f4cea3
David Sullivan <dsulli8247@gmail.com>
2025-04-07 19:05:35 -0400

    Updates to player and constants

--- player.py
+++ player.py
@@ -15,14 +15,20 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
-        
-
+   
+     
     def draw(self, screen):
         # sub-classes must override
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def update(self, dt):
-        # sub-classes must override
-        pass
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.rotation = self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotation = self.rotate(dt)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
 
 
